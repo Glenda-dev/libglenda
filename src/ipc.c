31db588c22206af1054dd496d4cc00ca1d7b37a6
2018wzh <wzh20070720@outlook.com>
2026-02-06 14:36:22 +0800

    [utcb] Use direct buffer

--- src/ipc.c
+++ src/ipc.c
@@ -4,22 +4,12 @@
 
 size_t utcb_available_space(glenda_utcb_t *utcb)
 {
-    return BUFFER_MAX_SIZE - utcb_available_data(utcb) - 1;
+    return BUFFER_MAX_SIZE - utcb->size;
 }
 
 size_t utcb_available_data(glenda_utcb_t *utcb)
 {
-    size_t head = utcb->head;
-    size_t tail = utcb->tail;
-
-    if (tail >= head)
-    {
-        return tail - head;
-    }
-    else
-    {
-        return BUFFER_MAX_SIZE - head + tail;
-    }
+    return utcb->size;
 }
 
 size_t utcb_write(glenda_utcb_t *utcb, const uint8_t *data, size_t len)
@@ -29,9 +19,9 @@ size_t utcb_write(glenda_utcb_t *utcb, const uint8_t *data, size_t len)
 
     for (size_t i = 0; i < to_write; i++)
     {
-        utcb->ipc_buffer[utcb->tail] = data[i];
-        utcb->tail = (utcb->tail + 1) % BUFFER_MAX_SIZE;
+        utcb->ipc_buffer[utcb->size + i] = data[i];
     }
+    utcb->size += to_write;
     return to_write;
 }
 
@@ -42,14 +32,12 @@ size_t utcb_read(glenda_utcb_t *utcb, uint8_t *data, size_t len)
 
     for (size_t i = 0; i < to_read; i++)
     {
-        data[i] = utcb->ipc_buffer[utcb->head];
-        utcb->head = (utcb->head + 1) % BUFFER_MAX_SIZE;
+        data[i] = utcb->ipc_buffer[i];
     }
     return to_read;
 }
 
 void utcb_clear(glenda_utcb_t *utcb)
 {
-    utcb->head = 0;
-    utcb->tail = 0;
+    utcb->size = 0;
 }
